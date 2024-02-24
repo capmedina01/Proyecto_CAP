698cc51d0b532fce5c390cc2ba5e6bbe17c2f9bc
capmedina01 <kalichecomfenalco@gmail.com>
2024-02-23 20:46:21 -0500

    java

---
+++ Proyectos_java/prueba.java
@@ -0,0 +1,11 @@
+package com.proyecto_prueba;
+
+public class Prueba
+{
+  //definicion de atributos
+  private String tamanio;
+  private String color;
+  private String material;
+  private Double costo;
+
+}

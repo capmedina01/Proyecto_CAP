f8462439ea7de862c04fd3eb10bde6597b7865b5
capmedina01 <kalichecomfenalco@gmail.com>
2024-02-23 20:57:21 -0500

    geter and seters

--- Proyectos_java/com/proyecto_prueba/prueba.java
+++ Proyectos_java/com/proyecto_prueba/prueba.java
@@ -7,6 +7,30 @@ public class Prueba
   private String color;
   private String material;
   private Double costo;
+  public String getTamanio() {
+    return tamanio;
+  }
+  public void setTamanio(String tamanio) {
+    this.tamanio = tamanio;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public String getMaterial() {
+    return material;
+  }
+  public void setMaterial(String material) {
+    this.material = material;
+  }
+  public Double getCosto() {
+    return costo;
+  }
+  public void setCosto(Double costo) {
+    this.costo = costo;
+  }
 
   
 
